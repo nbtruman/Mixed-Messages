8e8e60db36e5d20a0d3f275061ffa875421422a3
nbtruman <104936442+nbtruman@users.noreply.github.com>
2023-07-20 21:47:59 +0100

    write template string

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,5 +1,6 @@
 import { words } from "./resources/wordArrays.mjs";
 
-const horoscope = new words;
+const parameters = new words;
+const horoscope = `Today, ${parameters.sign} will experience a surge of ${parameters.positiveTrait} energy. It's a great time to take on new challenges and be more ${parameters.positiveTrait}. However, be mindful of your tendency to be ${parameters.negativeTrait}, as it may lead to unnecessary conflicts. Embrace the positive aspects of your personality and work on transforming the negative ones. This will help you achieve harmony in both your personal and professional life.`;
 
-console.log(horoscope.sign);
+console.log(horoscope);
